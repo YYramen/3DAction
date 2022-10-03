a332ebdf819218c832aacaf8ab5dade3eee48d5a
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-10-03 16:54:50 +0900

    ダッシュ（回避）機能の追加

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -8,11 +8,15 @@ using UnityEngine;
 /// </summary>
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField,Tooltip("移動速度")] float _moveSpeed = 1f;
+    [SerializeField, Tooltip("ダッシュ力")] float _dashPower = 5.0f;
+    [SerializeField, Tooltip("移動速度")] float _moveSpeed = 1.0f;
+    [SerializeField, Tooltip("ジャンプ力")] float _jumpPower = 5.0f;
+
     [SerializeField, Tooltip("カメラの位置")] Transform _aimPos;
     [SerializeField] AxisState _vertical;
     [SerializeField] AxisState _horizontal;
 
+    Rigidbody _rb;
     Animator _anim;
     int _hashFront = Animator.StringToHash("Front");
     int _hashSide = Animator.StringToHash("Side");
@@ -20,6 +24,7 @@ public class PlayerMove : MonoBehaviour
     private void Awake()
     {
         _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Start()
@@ -33,13 +38,14 @@ public class PlayerMove : MonoBehaviour
     }
 
     /// <summary>
-    /// 入力を受け付ける関数
+    /// 入力受け付け、移動速度、アニメーター関数
+    /// 移動方法は Velocity で行う
     /// </summary>
     private void InputMove()
     {
         // 入力を受け付ける
-        var x = Input.GetAxis("Horizontal");
-        var z = Input.GetAxis("Vertical");
+        var x = Input.GetAxisRaw("Horizontal");
+        var z = Input.GetAxisRaw("Vertical");
         var dir = new Vector3(x, 0, z).normalized;
         _horizontal.Update(Time.deltaTime);
         _vertical.Update(Time.deltaTime);
@@ -56,5 +62,21 @@ public class PlayerMove : MonoBehaviour
         // Animatorに値を反映
         _anim.SetFloat(_hashFront, velocity.z, 0.1f, Time.deltaTime);
         _anim.SetFloat(_hashSide, velocity.x, 0.1f, Time.deltaTime);
+
+        // ダッシュ
+        if (Input.GetButtonDown("Dash"))
+        {
+            Dash(dir);
+        }
+    }
+
+    /// <summary>
+    /// ダッシュ
+    /// </summary>
+    /// <param name="dir">ダッシュの方向</param>
+    private void Dash(Vector3 dir)
+    {
+        //_rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        _rb.AddForce(dir * _dashPower, ForceMode.Impulse);
     }
 }
