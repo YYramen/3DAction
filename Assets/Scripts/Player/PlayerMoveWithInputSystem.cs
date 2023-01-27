3935bb8a1f6e1e6bec0c73e6a6fe4e3e18306c9d
YUSUKE <yoshidayusuke1124@gmail.com>
2023-01-27 20:23:57 +0900

    PlayerMoveクラス修正中

--- Assets/Scripts/Player/PlayerMoveWithInputSystem.cs
+++ Assets/Scripts/Player/PlayerMoveWithInputSystem.cs
@@ -3,20 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// InputSystemを使用したPlayerの移動コンポーネント
+/// </summary>
 public class PlayerMoveWithInputSystem : MonoBehaviour
 {
+    [Header("移動速度")]
+    [SerializeField, Tooltip("移動速度")] float _moveSpeed;
+    [Tooltip("移動力を格納用しておく変数")] Vector3 _movement;
+
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _movement * _moveSpeed;
+    }
+
     public void OnPunch(InputAction.CallbackContext context)
     {
-        Debug.Log("パンチ");
+        if(context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("パンチ");
+        }
     }
 
     public void OnKick(InputAction.CallbackContext context)
     {
-        Debug.Log("キック");
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("キック");
+        }
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputValue value)
     {
-        Debug.Log("移動");
+        Vector2 movementVector = value.Get<Vector2>();
+        _movement = new Vector3(movementVector.x, 0.0f, movementVector.y);
     }
 }
